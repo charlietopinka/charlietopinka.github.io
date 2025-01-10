7ec789301beb05d92aeb925895402d551adbe354
charlietopinka <charlietopinka@gmail.com>
2025-01-09 20:48:30 -0500

    Create Exceptions.py

---
+++ Exceptions.py
@@ -0,0 +1,10 @@
+class SearchError(Exception):
+    pass
+class IllegalArgumentError(ValueError):
+    pass
+    
+class InsufficientFunds(Exception):
+    pass
+
+class InputError(Exception):
+    pass
